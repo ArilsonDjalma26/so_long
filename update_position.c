82b0ca80e546b26b3475aa0939c334b60eb9b68c
Arilson Djalma <arilsonalbano625@gmail.com>
2025-10-18 07:04:51 +0100

    movimento ao alcançar a saida

--- update_position.c
+++ update_position.c
@@ -20,7 +20,11 @@ void	update_position(t_game *game, int new_x, int new_y)
 	if (next == 'C')
 		game->collect_count--;
 	if (next == 'E' && game->collect_count == 0)
+	{
+		game->moves++;
+		ft_printf("Moves:%d\n", game->moves);
 		handle_close(game);
+	}
 	game->map[game->player_y][game->player_x] = '0';
 	game->map[new_y][new_x] = 'P';
 	game->player_y = new_y;
