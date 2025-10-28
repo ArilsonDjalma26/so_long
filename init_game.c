b40e8fcdf95b0508f70b2c573add66bd09de778b
Arilson <arilsonalbano625@gmail.com>
2025-10-28 18:41:44 +0100

    verificacao do tamanho do mapa

--- init_game.c
+++ init_game.c
@@ -6,7 +6,7 @@
 /*   By: aalbano <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 10:14:41 by aalbano           #+#    #+#             */
-/*   Updated: 2025/10/23 16:52:52 by aalbano          ###   ########.fr       */
+/*   Updated: 2025/10/28 18:21:13 by aalbano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 void	init_game(t_game *game)
 {
+	int		screen_x;
+	int		screen_y;
+
 	game -> mlx = mlx_init();
 	if (!game->mlx)
 		cleanup_and_exit(game, "Erro ao inicializar Minilibx!");
 	load_images(game);
-	game->mlx_win = mlx_new_window(game->mlx, game->width
-			* TILE_SIZE, game->height * TILE_SIZE, "so_long");
+	mlx_get_screen_size(game -> mlx, &screen_x, &screen_y);
+	if (game -> width * TILE_SIZE > screen_x
+		|| game ->height * TILE_SIZE > screen_y)
+		cleanup_and_exit(game, "Mapa maior do que tamanho da tela!");
+	game->mlx_win = mlx_new_window(game->mlx, game -> width
+			*TILE_SIZE, game ->height * TILE_SIZE, "so_long");
+	ft_printf("%dx%d\n", game -> width, game -> height);
 	if (!game->mlx_win)
 		cleanup_and_exit(game, "Erro ao criar janela!");
 	draw_map(game);
